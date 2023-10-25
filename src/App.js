563d07579d64dfdb8ad4f431aac23904c47edbc8
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-10-25 18:14:42 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import img from './logo420.png'
-import './App.css';
+import React from "react";
+import img from "./logo420.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={img} alt=""/>
+        <img src={img} alt="" />
       </header>
     </div>
   );
 }
 
 export default App;
+
